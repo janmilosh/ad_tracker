2997c9d1d9fba00510fcdbd276e70c1db0e8ef62
Jan Milosh <janmilosh@gmail.com>
2014-08-31 16:46:30 -0400

    Adds Ad and Newspaper summary list to home page.

--- ad_tracker/views.py
+++ ad_tracker/views.py
@@ -1,7 +1,14 @@
 from django.contrib.auth import logout
 from django.shortcuts import redirect, render
 
+from ads.models import Ad
+from newspapers.models import Newspaper
+
 def home(request):
+    ads = Ad.objects.all()
+    newspapers = Newspaper.objects.all()
+    
     return render(request, "home.html", ({
-        
+        'ads': ads,
+        'newspapers': newspapers,
     }))
