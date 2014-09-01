8857e6280549aba6ddd7b8a92071c352a7f81bfc
Jan Milosh <janmilosh@gmail.com>
2014-09-01 13:04:31 -0400

    Sets up login.

--- ad_tracker/urls.py
+++ ad_tracker/urls.py
@@ -4,8 +4,12 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', 'ad_tracker.views.login'),
+    url(r'^accounts/auth/$', 'ad_tracker.views.auth_view'),
+    url(r'^accounts/logout/$', 'ad_tracker.views.logout'),
+    url(r'^accounts/loggedin/$', 'ad_tracker.views.loggedin'),
+    url(r'^accounts/invalid/$', 'ad_tracker.views.invalid_login'),
     url(r'^$', 'ad_tracker.views.home', name='home'),
     url(r'^newspaper/', include('newspapers.urls')),
     url(r'^ad/', include('ads.urls')),
--- ad_tracker/views.py
+++ ad_tracker/views.py
@@ -1,5 +1,7 @@
+from django.shortcuts import redirect, render, render_to_response
+from django.contrib import auth
 from django.contrib.auth import logout
-from django.shortcuts import redirect, render
+from django.core.context_processors import csrf
 from django.utils import timezone
 import datetime
 
@@ -15,3 +17,31 @@ def home(request):
         'ads': ads,
         'newspapers': newspapers,
     }))
+
+def login(request):
+    c = {}
+    c.update(csrf(request))
+    return render_to_response('login/login.html', c)
+
+def auth_view(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(username=username, password=password)
+
+    if user is not None:
+        auth.login(request, user)
+        return redirect('/accounts/loggedin')
+    else:
+        return redirect('/accounts/invalid')
+
+def loggedin(request):
+    return render(request, 'login/loggedin.html', ({
+        'full_name': request.user.username
+    }))
+
+def invalid_login(request):
+    return render(request, 'login/invalid-login.html')
+
+def logout(request):
+    auth.logout(request)
+    return render(request, 'login/logout.html')
